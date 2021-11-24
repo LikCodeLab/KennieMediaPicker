7a2db8d52e9b78ee7c613c5bf27ced4279de7dd9
kennie <245794335@qq.com>
2021-11-24 10:14:24 +0800

    update

--- app_example/src/main/java/com/kennie/example/mediapicker/MainActivity.kt
+++ app_example/src/main/java/com/kennie/example/mediapicker/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.kennie.example.library.R
-import com.kennie.library.example.databinding.ActivityMainBinding
+import com.kennie.example.library.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
